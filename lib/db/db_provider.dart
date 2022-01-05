43f0596710baed4b0b2acb09fa6dbedb7878eec8
feroid765 <feroid765@gmail.com>
2022-01-05 22:49:24 +0900

    Add Db Provider

---
+++ lib/db/db_provider.dart
@@ -0,0 +1,29 @@
+import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart';
+
+class DbProvider {
+  static Database? db;
+  static const String fileName = 'feroid_phone-stick_database.db';
+
+  DbProvider();
+
+  static Future _onCreate(Database db, int version) async {
+    await db.execute(
+        'CREATE TABLE stick(id TEXT PRIMARY KEY, name TEXT NOT NULL, light_list TEXT NOT NULL)');
+  }
+
+  Future _init() async {
+    if (db?.isOpen == true) return;
+    var databasePath = await getDatabasesPath();
+    var fullPath = path.join(databasePath, fileName);
+
+    db = await openDatabase(fullPath, version: 1, onCreate: _onCreate);
+  }
+
+  static Future<DbProvider> create() async {
+    var result = DbProvider();
+    await result._init();
+
+    return result;
+  }
+}
