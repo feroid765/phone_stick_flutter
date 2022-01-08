565401ca91fd1178fba187f18860111b081cb316
feroid765 <feroid765@gmail.com>
2022-01-09 02:42:09 +0900

    Working on Add Stick Page

--- lib/page_index/add_stick.dart
+++ lib/page_index/add_stick.dart
@@ -1,14 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'main_my_sticks.dart';
+import '../models/light.dart';
+
+extension LightToWidget on Light {
+  ListTile getWidget() {
+    return ListTile(
+        title: Text(name),
+        leading: Container(
+          height: 24.0,
+          width: 24.0,
+          color: color
+        )
+    );
+  }
+}
 
 class AddStickPage extends StatefulWidget {
   const AddStickPage({Key? key}) : super(key: key);
+
   @override
   _AddStickPageState createState() => _AddStickPageState();
 }
 
-class _AddStickPageState extends State<AddStickPage>{
+class _AddStickPageState extends State<AddStickPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  List<Light> _lightList = [];
 
   @override
   void initState() {
@@ -26,7 +43,23 @@ class _AddStickPageState extends State<AddStickPage>{
       appBar: AppBar(
         title: const Text('폰광봉 만들기'),
       ),
-      body: const Text('Hi'),
+      body: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            const Text('폰광봉 이름'),
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: "폰광봉의 이름을 입력해주세요!"
+              )
+            ),
+            const Divider(),
+            const Text('색깔 목록'),
+            ListView(children: _lightList.map((ll) => ll.getWidget()).toList())
+          ]
+        )
+      ),
     );
   }
 }
